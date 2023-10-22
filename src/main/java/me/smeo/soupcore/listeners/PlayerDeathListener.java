eec6f5a10e6a53ce85b0c7cb61ad93089d0d2f29
Sme0 <83521328+Sme0@users.noreply.github.com>
2023-10-22 12:27:46 +0100

    Added death messages and killstreak milestone broadcasts

--- src/main/java/me/smeo/soupcore/listeners/PlayerDeathListener.java
+++ src/main/java/me/smeo/soupcore/listeners/PlayerDeathListener.java
@@ -1,6 +1,7 @@
 package me.smeo.soupcore.listeners;
 
 import me.smeo.soupcore.Database.Database;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,6 +16,12 @@ public class PlayerDeathListener implements Listener
         Player p = e.getEntity();
         Database.SetPlayerData(p, "deaths", (Database.getPlayerData(p, "deaths") + 1));
         Database.SetPlayerData(p, "killStreak", 0);
+
+        int killStreak = Database.getPlayerData(p, "killStreak");
+        if(killStreak >= 20)
+        {
+            e.setDeathMessage(ChatColor.RED + p.getName() + ChatColor.GRAY + " has died with a killstreak of " + ChatColor.AQUA + killStreak);
+        }
         e.getEntity().spigot().respawn();
     }
 
