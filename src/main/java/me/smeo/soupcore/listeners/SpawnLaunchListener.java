af673242ca74da80f83844e040f8ca5f455d190a
Donut-code <acrowe707@gmail.com>
2024-07-13 11:51:56 +0100

    style: remove obsolete imports

--- src/main/java/me/smeo/soupcore/listeners/SpawnLaunchListener.java
+++ src/main/java/me/smeo/soupcore/listeners/SpawnLaunchListener.java
@@ -1,9 +1,14 @@
 package me.smeo.soupcore.listeners;
 
+import com.sk89q.worldguard.bukkit.RegionContainer;
+import com.sk89q.worldguard.bukkit.RegionQuery;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import me.smeo.soupcore.SoupCore;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -45,18 +50,31 @@ public class SpawnLaunchListener implements Listener {
     public void onPressurePlate(PlayerMoveEvent e)
     {
         Player p = e.getPlayer();
-        if(p.getLocation().subtract(0, 1, 0).getBlock().getType() == Material.WOOL)
-        {
-            p.playSound(p.getLocation(), Sound.EXPLODE, 8, 1);
-            p.setVelocity(new Vector(1 + p.getLocation().getDirection().getX() * 6, 2, 1 + p.getLocation().getDirection().getZ() * 6));
-            cancelFallDamage.add(p.getUniqueId());
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    cancelFallDamage.remove(p.getUniqueId());
-                }
-            }.runTaskLaterAsynchronously(SoupCore.plugin, 20L * 10L);
+
+        RegionContainer container = SoupCore.getWorldGuard.getRegionContainer();
+        RegionManager regions = container.get(p.getWorld());
+        assert regions != null;
+        ProtectedRegion region = regions.getRegion("spawn");
+
+        Location loc = p.getLocation();
+        RegionQuery query = container.createQuery();
+        ApplicableRegionSet set = query.getApplicableRegions(loc);
+
+        if (!set.getRegions().contains(region)) {
+            return;
         }
+        if(p.getLocation().subtract(0, 1, 0).getBlock().getType() != Material.WOOL) {
+            return;
+        }
+
+        p.playSound(p.getLocation(), Sound.EXPLODE, 8, 1);
+        p.setVelocity(new Vector(1 + p.getLocation().getDirection().getX() * 6, 2, 1 + p.getLocation().getDirection().getZ() * 6));
+        cancelFallDamage.add(p.getUniqueId());
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                cancelFallDamage.remove(p.getUniqueId());
+            }
+        }.runTaskLaterAsynchronously(SoupCore.plugin, 20L * 10L);
     }
 }
-
