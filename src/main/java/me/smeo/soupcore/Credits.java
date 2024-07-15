1d7a6b01456d4bacd6d1d25b38d5b2c092405590
Donut-code <acrowe707@gmail.com>
2024-07-15 21:55:15 +0100

    style: simplify return statement

--- src/main/java/me/smeo/soupcore/Credits.java
+++ src/main/java/me/smeo/soupcore/Credits.java
@@ -3,27 +3,25 @@ package me.smeo.soupcore;
 import me.smeo.soupcore.Database.Database;
 import org.bukkit.entity.Player;
 
+import java.util.Objects;
+
 public class Credits
 {
 
     public static boolean checkCreditBalance(Player player, Integer amount) // Checks if user has enough credits
     {
-        if(Integer.valueOf((String) Database.getPlayerData(player, "soupData", "credits")) >= amount)
-        {
-            return true;
-        }
-        return false;
+        return Integer.parseInt((String) Objects.requireNonNull(Database.getPlayerData(player, "soupData", "credits"))) >= amount;
     }
 
     public static void giveCredits(Player player, int amount)
     {
-        int newBalance = Integer.valueOf((String) Database.getPlayerData(player, "soupData", "credits")) + amount;
+        int newBalance = Integer.parseInt((String) Objects.requireNonNull(Database.getPlayerData(player, "soupData", "credits"))) + amount;
         Database.SetPlayerData(player, "soupData", "credits", newBalance);
     }
 
     public static void chargeCredits(Player player, int amount)
     {
-        int currentBalance = Integer.valueOf((String) Database.getPlayerData(player, "soupData", "credits"));
+        int currentBalance = Integer.parseInt((String) Objects.requireNonNull(Database.getPlayerData(player, "soupData", "credits")));
         Database.SetPlayerData(player, "soupData", "credits", currentBalance-amount);
     }
 
