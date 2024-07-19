66d910ca945c2a9a132b395607c0eede434cebb1
Donut-code <acrowe707@gmail.com>
2024-07-19 21:41:22 +0100

    feat: add health below names

---
+++ src/main/java/org/genesismc/SoupCore/listeners/scoreboardListeners.java
@@ -0,0 +1,27 @@
+package org.genesismc.SoupCore.listeners;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.*;
+
+public class scoreboardListeners implements Listener {
+    private static Objective showHearts;
+    public static void enableHeartsBelowName() {
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getNewScoreboard();
+
+        showHearts = board.registerNewObjective("showhearts", "health");
+        showHearts.setDisplaySlot(DisplaySlot.BELOW_NAME);
+        showHearts.setDisplayName(ChatColor.DARK_RED + "❤");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        p.setScoreboard(showHearts.getScoreboard());
+    }
+}
